b3ff1316811f420c3e8c60a6079dbd6029a64dce
Adam Bako <bakoad@gmail.com>
2021-04-12 12:14:25 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='datadirtest',
-    version='0.0.8',
+    version='1.0.1',
     setup_requires=['setuptools_scm'],
     url='https://bitbucket.org/kds_consulting_team/data-dir-test',
     download_url='https://bitbucket.org/kds_consulting_team/data-dir-test',
